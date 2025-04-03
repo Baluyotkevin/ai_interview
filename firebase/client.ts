6f065d53a395bf3d703e7c319442ecfd4956caef
Kevin Baluyot <baluyotkevin33@yahoo.com>
2025-04-02 19:03:48 -0700

    fixed

--- firebase/client.ts
+++ firebase/client.ts
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase-admin/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCeuNgxHkJ7m1JY6V6IxB5ZIyN-idiGlyM",
-  authDomain: "kbaiprep.firebaseapp.com",
-  projectId: "kbaiprep",
-  storageBucket: "kbaiprep.firebasestorage.app",
-  messagingSenderId: "620353679380",
-  appId: "1:620353679380:web:840d4eaa303e3a52e0b701",
-  measurementId: "G-5CPSHPY22V"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
